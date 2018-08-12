ac10608f7fb24856a5a659dce487049bdbdc023f
Nyiro Gergo <gergo.nyiro@gmail.com>
2018-08-12 22:27:39 +0200

    client: get command with wreq and lense
    The wreq http library is used for the client application because the
    flexibility of lense is required by our interactive scenario.
    
    The command parsing is made by ReadP monad. The tips and tricks come
    from:
     * https://two-wrongs.com/parser-combinators-parsing-for-haskell-beginners.html
     * http://cmsc-16100.cs.uchicago.edu/2017/Lectures/18/practical-parsing.php

---
+++ src/Client.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Lens
+import Network.HTTP.Client
+import Data.Aeson.Lens (key, _String, _Object)
+import Text.ParserCombinators.ReadP
+
+import qualified Network.Wreq as W
+import qualified Control.Exception as E
+import qualified Data.ByteString.Char8 as BSC
+import qualified Data.ByteString.Lazy as LBS
+import qualified Network.Wreq.Session as S
+import qualified Data.Text as T
+
+
+server_url = "http://localhost:3000/"
+
+data Command = Get { url :: String } deriving Show
+
+readCommand :: ReadP Command
+readCommand = do
+    string "get"
+    string " "
+    url <- munch (\char -> char >= 'a' && char <= 'z')
+    return (Get url)
+
+type CommandResult = Either String Command
+
+parseMaybe :: ReadP Command -> String -> CommandResult
+parseMaybe parser input =
+    case readP_to_S parser input of
+        [] -> Left "Wrong command"
+        ((r, _): _) -> Right r
+
+loop session stack = do
+    line <- getLine
+    case parseMaybe readCommand line of
+        Left msg -> do
+            print msg
+            loop session stack
+        Right (Get u) -> do
+            result <- safeGet session (server_url ++ u)
+            case result of
+                Right response -> do
+                    let body = response ^. W.responseBody
+                    print body
+                    loop session (body:stack)
+                Left msg -> do
+                    print msg
+                    loop session stack
+
+
+type ResponseResult = Either String (Response LBS.ByteString)
+
+safeGet :: S.Session -> String -> IO ResponseResult
+safeGet session url = (Right <$> S.get session url) `E.catch` handler
+
+handler :: HttpException -> IO ResponseResult
+handler (HttpExceptionRequest _ (StatusCodeException r _)) = return $ Left $ BSC.unpack (r ^. W.responseStatus . W.statusMessage)
+
+
+main :: IO ()
+main = do
+    session <- S.newSession
+    let stack = []
+    loop session stack
